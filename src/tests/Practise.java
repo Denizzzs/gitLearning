33b8c3be03ea12c2ba4d30c03c42606d7107ea25
“Asiya59” <sistedeniz.1920@gmail.com>
2020-05-12 16:49:14 +0200

    githuba gonder

---
+++ src/tests/Practise.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class Practise {
+    public void(){
+        //hello tuesday
+    }
+}
--- src/tests/Winter.java
+++ src/tests/Winter.java
@@ -3,5 +3,6 @@ package tests;
 public class Winter {
     public void(){
         System.out.println("hello github");
+        System.out.println("git push button on the right up");
     }
 }
