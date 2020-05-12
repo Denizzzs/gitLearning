6f83e1a3ea92d4884a1e796c76b94be6373f7fc9
Denizzzs <62035510+Denizzzs@users.noreply.github.com>
2020-05-12 17:06:53 +0200

    Update Practise.java

--- src/tests/Practise.java
+++ src/tests/Practise.java
@@ -3,5 +3,10 @@ package tests;
 public class Practise {
     public void(){
         //hello tuesday
+        
+        
+        
+        // i am pulling now 
+        //this line is edited by github
     }
 }
